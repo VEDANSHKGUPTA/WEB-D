6629b705785e7ce4af7ddb25cba397eb23f26d74
VEDANSHKGUPTA <dollarvedansh@gmail.com>
2019-02-24 02:03:47 +0530

    vedansh

--- DESPECABLES.ino
+++ DESPECABLES.ino
@@ -30,4 +30,17 @@ void loop() {
    digitalWrite(LED3,HIGH);
    delay(5000);
   }
+    else if(LDRValue<=700&&LDRValue>=300)
+  {
+   digitalWrite(LED1,HIGH);
+   digitalWrite(LED4,HIGH);
+   digitalWrite(LED2,LOW);
+   digitalWrite(LED3,LOW);
+   delay(10000);
+   digitalWrite(LED1,LOW);
+   digitalWrite(LED4,LOW);
+   digitalWrite(LED2,HIGH);
+   digitalWrite(LED3,HIGH);
+   delay(10000);
+  }
 }
